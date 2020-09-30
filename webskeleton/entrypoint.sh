39a19f3f756af7aafc3c25fd529c9bd080b09cdb
RafalMalik <rafalmalik.info@gmail.com>
2020-09-30 23:05:50 +0200

    Added doctrine command to webskeleton entrypoint.sh

--- webskeleton/entrypoint.sh
+++ webskeleton/entrypoint.sh
@@ -11,6 +11,14 @@ ServerName localhost \
 # INSTALL COMPOSER DEP #
 composer install
 
+# RUN MIGRATIONS AND FIXTURES
+php bin/console doctrine:database:create --no-interaction --if-not-exists
+php bin/console doctrine:migrations:migrate --no-interaction
+php bin/console assets:install --symlink --env=prod
+
+# LOAD FIXTURES
+php -d memory_limit=-1 bin/console doctrine:fixtures:load --no-interaction
+
 # COMPILE FRONT ASSETS #
 yarn install
 yarn encore dev
